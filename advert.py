2c640445bb616c63c7185083be139aa05dabe7b7
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-01-02 19:29:17 -0700

    Create a new dataframe by selecting the first 7 rows of the current dataframe

--- advert.py
+++ advert.py
@@ -8,3 +8,8 @@ data_filename = 'Advertising.csv'
 
 # Read the file "Advertising.csv" file using the pandas library
 df = pd.read_csv(data_filename)
+df.head()
+
+# Create a new dataframe by selecting the first 7 rows of the current dataframe
+df_new = df.head(7)
+print(df_new)
