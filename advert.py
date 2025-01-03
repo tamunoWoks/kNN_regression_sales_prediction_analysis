bc3b6f69bd629e48d8173ba4a02500bef671f9d3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-01-02 19:27:59 -0700

    Read the file "Advertising.csv" file using the pandas library

--- advert.py
+++ advert.py
@@ -2,3 +2,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+
+data_filename = 'Advertising.csv'
+
+# Read the file "Advertising.csv" file using the pandas library
+df = pd.read_csv(data_filename)
